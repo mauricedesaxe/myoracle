3df5e357cb3655d51dfcafc1095468e0f6616f3b
lazaralex98 <alex2012ro@gmail.com>
2024-07-21 23:47:03 +0300

    every 3 seconds get the price and log it

--- main.go
+++ main.go
@@ -4,8 +4,17 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
+
+var asset = "ETH"
+var denomination = "USD"
+
+var coinGecko = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" + denomination +
+	"&ids=" + asset + "&order=market_cap_by_total_volume&per_page=100&page=1&sparkline=false&price_change_percentage=24h&locale=en"
+var coinMarketCap = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=" + asset + "&convert=" + denomination
 
 func main() {
 	link := flag.String("link", "", "The url of the first node to sync to, will be used to sync the other nodes")
@@ -17,6 +26,23 @@ func main() {
 		panic("error first syncing nodes: " + err.Error())
 	}
 	fmt.Println("First synced to:", len(nodes), "nodes")
+
+	for {
+		time.Sleep(3 * time.Second)
+
+		price1, err := getPriceFromCoinGecko()
+		if err != nil {
+			log.Println("error getting price from coin gecko: " + err.Error())
+		}
+
+		price2, err := getPriceFromCoinMarketCap()
+		if err != nil {
+			log.Println("error getting price from coin market cap: " + err.Error())
+		}
+
+		median := (price1 + price2) / 2
+		fmt.Println("Median price:", median)
+	}
 }
 
 func requestNodes(node string) ([]string, error) {
@@ -40,4 +66,38 @@ func requestNodes(node string) ([]string, error) {
 		return nil, err
 	}
 	return response.Nodes, nil
+}
+
+func getPriceFromCoinGecko() (float64, error) {
+	resp, err := http.Get(coinGecko)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	type Response struct {
+		Price float64 `json:"price"`
+	}
+	var response Response
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return 0, err
+	}
+	return response.Price, nil
+}
+
+func getPriceFromCoinMarketCap() (float64, error) {
+	resp, err := http.Get(coinMarketCap)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	type Response struct {
+		Price float64 `json:"price"`
+	}
+	var response Response
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return 0, err
+	}
+	return response.Price, nil
 }
